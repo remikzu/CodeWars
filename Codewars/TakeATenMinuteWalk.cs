5069dc760198bdd0bbf92d44a94ac7e94e2eabfb
Remigiusz Zudzin <remik.zudzin@gmail.com>
2020-03-23 08:31:45 +0100

    Take a Ten Minute Walk Kata submitted

---
+++ Codewars/TakeATenMinuteWalk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codewars
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/54da539698b8a2ad76000228/train/csharp
+    /// Take a Ten Minute Walk
+    /// </summary>
+    public class TakeATenMinuteWalk
+    {
+        public static bool IsValidWalk(string[] walk)
+        {
+            int sumN = 0;
+            int sumS = 0;
+            int sumE = 0;
+            int sumW = 0;
+
+            for (int i = 0; i < walk.Length; i++)
+            {
+                if (walk[i] == "n") sumN++;
+                else if (walk[i] == "s") sumS++;
+                else if (walk[i] == "e") sumE++;
+                else if (walk[i] == "w") sumW++;
+            }
+
+            return (sumN == sumS && sumE == sumW && walk.Length > 0 && walk.Length == 10);
+        }
+    }
+}
---
+++ Tests/TakeATenMinuteWalkTest.cs
@@ -0,0 +1,33 @@
+﻿using Codewars;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    public class TakeATenMinuteWalkTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, TakeATenMinuteWalk.IsValidWalk(new string[] { "n", "s", "n", "s", "n", "s", "n", "s", "n", "s" }), "should return true");
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(false, TakeATenMinuteWalk.IsValidWalk(new string[] { "w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e" }), "should return false");
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(false, TakeATenMinuteWalk.IsValidWalk(new string[] { "w" }), "should return false");
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(false, TakeATenMinuteWalk.IsValidWalk(new string[] { "n", "n", "n", "s", "n", "s", "n", "s", "n", "s" }), "should return false");
+        }
+
+    }
+}
