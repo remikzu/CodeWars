9038d1129a3b24d47d1f25aa3b539b3b0eecfd42
Remigiusz Zudzin <remik.zudzin@gmail.com>
2020-03-25 12:28:44 +0100

    Shortest Word Kata submitted

---
+++ Codewars/ShortestWord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codewars
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/csharp
+    /// Shortest Word
+    /// </summary>
+    public class ShortestWord
+    {
+        public static int FindShort(string s)
+        {
+            return s.Split(' ').OrderBy(w => w.Length).ToList().First().Length;
+        }
+    }
+}
---
+++ Tests/ShortestWordTest.cs
@@ -0,0 +1,18 @@
+﻿using Codewars;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    class ShortestWordTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(3, ShortestWord.FindShort("bitcoin take over the world maybe who knows perhaps"));
+            Assert.AreEqual(3, ShortestWord.FindShort("turns out random test cases are easier than writing out basic ones"));
+        }
+    }
+}
