ab222a25e1ed2f31b788f3a94d52f95632057d59
Remigiusz Zudzin <remik.zudzin@gmail.com>
2020-03-25 11:26:36 +0100

    Equal Sides Of An Array Kata submitted

---
+++ Codewars/EqualSidesOfAnArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codewars
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/csharp
+    /// Equal Sides Of An Array
+    /// </summary>
+    public class EqualSidesOfAnArray
+    {
+        public static int FindEvenIndex(int[] arr)
+        {
+            var sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            if (sum == 0) return 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {                
+                var sumRight = 0;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    sumRight += arr[j];
+                }
+
+                var sumLeft = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    sumLeft += arr[j];
+                }
+
+                if (sumRight == sumLeft) return i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Tests/EqualSidesOfAnArrayTest.cs
@@ -0,0 +1,38 @@
+﻿using Codewars;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    class EqualSidesOfAnArrayTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(3, EqualSidesOfAnArray.FindEvenIndex(new int[] { 1, 2, 3, 4, 3, 2, 1 }));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1, EqualSidesOfAnArray.FindEvenIndex(new int[] { 1, 100, 50, -51, 1, 1 }));
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(-1, EqualSidesOfAnArray.FindEvenIndex(new int[] { 1, 2, 3, 4, 5, 6 }));
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(3, EqualSidesOfAnArray.FindEvenIndex(new int[] { 20, 10, 30, 10, 10, 15, 35 }));
+        }
+        [Test]
+        public void Test5()
+        {
+            Assert.AreEqual(0, EqualSidesOfAnArray.FindEvenIndex(new int[] { 20, 10, -80, 10, 10, 15, 35 }));
+        }
+
+    }
+}
