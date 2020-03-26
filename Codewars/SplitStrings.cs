b68cfad87ed834167629aa74bedb1cc2ecf4dd36
Remigiusz Zudzin <remik.zudzin@gmail.com>
2020-03-26 07:35:47 +0100

    SplitString - unlocked a solution, couldn't resolve

---
+++ Codewars/SplitStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Codewars
+{
+    public class SplitStrings
+    {
+        public static string[] Solution(string str)
+        {
+            if (str.Length % 2 == 1)
+                str += "_";
+
+            List<string> list = new List<string>();
+            for (int i = 0; i < str.Length; i += 2)
+            {
+                list.Add(str[i].ToString() + str[i + 1]);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ Tests/SplitStringTest.cs
@@ -0,0 +1,22 @@
+﻿using Codewars;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    class SplitStringTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(new string[] { "ab", "c_" }, SplitStrings.Solution("abc"));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(new string[] { "ab", "cd", "ef" }, SplitStrings.Solution("abcdef"));
+        }
+    }
+}
