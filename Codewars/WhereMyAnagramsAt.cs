59fab09ca6002bd0deecf5f9ca2d3423331f2a65
Remigiusz Zudzin <remik.zudzin@gmail.com>
2020-04-01 17:30:58 +0200

    Where my anagrams at? Kata submitted. After posting correct solution added shortest version out of answers.

---
+++ Codewars/WhereMyAnagramsAt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codewars
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/csharp
+    /// Where my anagrams at?
+    /// </summary>
+    public class WhereMyAnagramsAt
+    {
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            var wordToChars = String.Concat(word.OrderBy(c => c));
+
+            for (int i = 0; i < words.Count; ++i)
+            {
+                if (wordToChars.Length != words[i].Length)
+                {
+                    words.RemoveAt(i);
+                    i--;
+                }
+                else
+                {
+                    var wordToCheck = String.Concat(words[i].OrderBy(c => c));
+                    if (!wordToChars.SequenceEqual(wordToCheck))
+                    {
+                        words.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            
+            return words;
+        }
+
+        //Shortest version
+        /*public static List<string> Anagrams(string word, List<string> words)
+        {
+            var pattern = word.OrderBy(p => p).ToArray();
+            return words.Where(item => item.OrderBy(p => p).SequenceEqual(pattern)).ToList();
+        }*/
+
+    }
+}
---
+++ Tests/WhereMyAnagramsAtTest.cs
@@ -0,0 +1,22 @@
+﻿using Codewars;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    class WhereMyAnagramsAtTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(new List<string> { "carer", "arcre", "carre" }, WhereMyAnagramsAt.Anagrams("racer", new List<string> { "carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr" }));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(new List<string> { "a" }, WhereMyAnagramsAt.Anagrams("a", new List<string> { "a", "b", "c", "d" }));
+        }
+    }
+}
