b50d9c075c92bbfa4eaad943c453ba40b231d23e
Remigiusz Zudzin <remik.zudzin@gmail.com>
2020-04-02 14:36:04 +0200

    Directions Reduction Kata

---
+++ Codewars/DirectionsReduction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/// <summary>
+/// https://www.codewars.com/kata/550f22f4d758534c1100025a/csharp
+/// Directions Reduction
+/// </summary>
+namespace Codewars
+{
+    public class DirectionsReduction
+    {
+        public static string[] dirReduc(String[] arr)
+        {
+            Stack<String> stack = new Stack<String>();
+
+            foreach (String direction in arr)
+            {
+                String lastElement = stack.Count > 0 ? stack.Peek().ToString() : null;
+
+                switch(direction)
+                {
+                    case "NORTH": 
+                        if ("SOUTH".Equals(lastElement))
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            stack.Push(direction);
+                        }
+                        break;
+                    case "SOUTH":
+                        if ("NORTH".Equals(lastElement))
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            stack.Push(direction);
+                        }
+                        break;
+                    case "EAST":
+                        if ("WEST".Equals(lastElement))
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            stack.Push(direction);
+                        }
+                        break;
+                    case "WEST":
+                        if ("EAST".Equals(lastElement))
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            stack.Push(direction);
+                        }
+                        break;
+                }
+            }
+            String[] result = stack.ToArray();
+            Array.Reverse(result);
+
+            return result;
+        }
+    }
+}
---
+++ Tests/DirectionReductionTest.cs
@@ -0,0 +1,26 @@
+﻿using Codewars;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    class DirectionReductionTest
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+            string[] b = new string[] { "WEST" };
+            Assert.AreEqual(b, DirectionsReduction.dirReduc(a));
+        }
+        [Test]
+        public void Test2()
+        {
+            string[] a = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            string[] b = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            Assert.AreEqual(b, DirectionsReduction.dirReduc(a));
+        }
+    }
+}
